66201e1a2bd5b39b82ba4ac7acead85e1abfd89c
Sgeo <sgeoster@gmail.com>
2019-12-17 23:25:06 -0500

    BrowserFS viewer. Creates new document from within tab where it's run.

--- inject.js
+++ inject.js
@@ -1,5 +1,46 @@
-const URL = "https://gitcdn.xyz/repo/Sgeo/BrowserFS_viewer/master/viewer.htm";
-(async function() {
-  let body = async fetch(URL).then(resp => resp.text());
-  
+(function() {
+    
+    let remoteWindow = window.open("about:blank");
+    let remoteDocument = remoteWindow.document;
+    
+    function showDir(parent, dirname, rootElem) {
+        let thisPath = FS.joinPath([parent, dirname]);
+        let entries = FS.readdir(thisPath).filter(entry => entry !== "." && entry !== "..");
+        console.log(entries);
+        for(let entry of entries) {
+            let li = remoteDocument.createElement("li");
+            let fullEntryPath = FS.joinPath([thisPath, entry]);
+            try {
+                if(FS.isDir(FS.stat(fullEntryPath).mode)) {
+                    li.textContent = entry;
+                    let ul = remoteDocument.createElement("ul");
+                    li.appendChild(ul);
+                    showDir(thisPath, entry, ul);
+                } else {
+                    try { 
+                        let blob = new Blob([FS.readFile(fullEntryPath)], {type: "application/octet-stream"});
+                        let url = remoteWindow.URL.createObjectURL(blob);
+                        let a = remoteDocument.createElement("a");
+                        a.textContent = entry;
+                        a.href = url;
+                        a.download = entry;
+                        li.appendChild(a);
+                    } catch(e) {
+                        li.textContent = entry; // Unable to read "file"
+                    }
+                }
+            } catch(e) {
+                console.error("Unable to descend", fullEntryPath, ": ", e);
+                li.textContent = entry;
+            }
+            rootElem.appendChild(li);
+        }
+    }
+    
+    let rootUl = remoteDocument.createElement("ul");
+    remoteDocument.body.appendChild(rootUl);
+    showDir("", "/", rootUl);
+
+    
+    
 })();
